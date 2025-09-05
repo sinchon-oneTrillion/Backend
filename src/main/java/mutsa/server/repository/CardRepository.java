d7e957482b947d5dbfe395d1019856b543c1bb78
donghoong <slimgreg5208@gmail.com>
2025-09-06 07:32:23 +0900

    충돌 해결

--- src/main/java/mutsa/server/repository/CardRepository.java
+++ src/main/java/mutsa/server/repository/CardRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
