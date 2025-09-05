cdca0087e54834ec764e1de0299b1a3600e8b18c
Rossenzii <jangmj80@naver.com>
2025-09-06 04:05:25 +0900

    [fix] controller name

--- src/main/java/mutsa/server/controller/CardController.java
+++ src/main/java/mutsa/server/controller/CardController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/cards")
 @RequiredArgsConstructor
-public class CardContoller {
+public class CardController {
     private final CardService cardService;
     private final UsersRepository usersRepository;
 
